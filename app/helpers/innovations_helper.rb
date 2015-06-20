dc0dfed2125587c04190694ddf445288b115e10d
BV Satyaram <bvsatyaram@gmail.com>
2015-06-20 05:50:35 +0530

    Handling the schenario where the applciation is not yet submitted

--- app/helpers/innovations_helper.rb
+++ app/helpers/innovations_helper.rb
@@ -1,5 +1,9 @@
 module InnovationsHelper
   def innovation_submission_date(innovation)
-    innovation.submitted_at.strftime("%I:%M %p on %b %e, %Y IST")
+    if innovation.submitted_at
+      return innovation.submitted_at.strftime("%I:%M %p on %b %e, %Y IST")
+    else
+      return "Not yet submitted"
+    end
   end
 end
