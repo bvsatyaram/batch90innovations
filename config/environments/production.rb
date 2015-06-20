03c909ff74ede9dab1df47a9db7e052f51946018
BV Satyaram <bvsatyaram@gmail.com>
2015-06-20 05:35:33 +0530

    Setting up credentials for email delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,16 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  # Mandrill SMTP Config
+  ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'batch90innovations.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
