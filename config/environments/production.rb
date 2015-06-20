c315c6a84d42a59f1e82c1dfe164eb126421bdd0
BV Satyaram <bvsatyaram@gmail.com>
2015-06-20 05:39:03 +0530

    Updating host for emails with custom domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
     :authentication => :plain
   }
   ActionMailer::Base.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'batch90innovations.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.batch90innovations.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
